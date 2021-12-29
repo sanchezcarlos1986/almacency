17f1a37a92be67e5772d7c15f07f7b9e5d55ddbf
Carlos Sanchez <carlossanchez.globant@latam.com>
2021-12-29 13:08:28 -0300

    feat: first commit ()

--- src/utils/currencyFormatter.test.ts
+++ src/utils/currencyFormatter.test.ts
@@ -3,9 +3,9 @@ import {currencyFormatter} from './currencyFormatter';
 describe('currencyFormatter', () => {
   test('returns a formatted currency', () => {
     expect(currencyFormatter({price: 100})).toEqual('$100.00');
-    expect(currencyFormatter({price: 100, currency: 'JPY'})).toBe('¥100');
-    expect(currencyFormatter({price: 100, currency: 'USD'})).toBe('$100.00');
-    expect(currencyFormatter({price: 100, currency: 'EUR'})).toBe('€100.00');
-    expect(currencyFormatter({price: 100, currency: 'CLP'})).toBe('CLP 100');
+    expect(currencyFormatter({price: 100, currency: 'JPY'})).toEqual('¥100');
+    expect(currencyFormatter({price: 100, currency: 'USD'})).toEqual('$100.00');
+    expect(currencyFormatter({price: 100, currency: 'EUR'})).toEqual('€100.00');
+    expect(currencyFormatter({price: 100, currency: 'CLP'})).toEqual('CLP 100');
   });
 });
